7384d2128a34a75f177d2942ca07319a13a1a007
Bryan Reymonenq <bryan.reymonenq@gmail.com>
2015-03-18 15:18:14 +0100

    Initial Commit

---
+++ FlappyBird/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlappyBird
+//
+//  Created by Bryan Reymonenq on 18/03/2015.
+//  Copyright (c) 2015 Fliizweb.fr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
