1b8750d83259da4f3f046a45123431920776692b
Jonathan <rcdsm@iJonathan.imerir.lan>
2015-03-19 13:37:34 +0100

    Ajout tableview + boutons sur le jeu

---
+++ ScoresTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ScoresTableViewController.h
+//  FlappyBird
+//
+//  Created by Jonathan on 19/03/2015.
+//  Copyright (c) 2015 Fliizweb.fr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScoresTableViewController : UITableViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
