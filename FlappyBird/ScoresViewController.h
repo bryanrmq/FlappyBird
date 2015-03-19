5ad41895c97d09f4eae27424b1ef5d13ba321440
Jonathan <rcdsm@iJonathan.imerir.lan>
2015-03-19 16:21:53 +0100

    Ajout de view score

--- FlappyBird/ScoresViewController.h
+++ FlappyBird/ScoresViewController.h
@@ -5,12 +5,11 @@
 //  Created by Jonathan on 19/03/2015.
 //  Copyright (c) 2015 Fliizweb.fr. All rights reserved.
 //
-
 #import <UIKit/UIKit.h>
 
 @interface ScoresViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 - (IBAction)backButton:(id)sender;
-
+- (void) addScore:(NSUInteger)value;
 @end
--- FlappyBird/ScoresViewController.m
+++ FlappyBird/ScoresViewController.m
@@ -12,29 +12,76 @@
 
 @end
 
-@implementation ScoresViewController
+@implementation ScoresViewController{
+    NSArray* paths;
+    NSString* documentsDirectoryPath;
+    NSString* filePath;
+    NSMutableDictionary* newList;
+    NSDictionary* newDiction;
+}
 
+-(BOOL)prefersStatusBarHidden{
+    return YES;
+}
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
+    
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
+    
+    paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    documentsDirectoryPath = paths.firstObject;
+    filePath = [documentsDirectoryPath stringByAppendingPathComponent:@"scores.plist"];
+    newList = [[NSMutableDictionary alloc] init];
+    
+    /// On récupère toutes les données du fichier .plist
+    if ([[NSFileManager defaultManager] fileExistsAtPath:filePath]) {
+        NSMutableDictionary* savedData = [NSMutableDictionary dictionaryWithContentsOfFile:filePath];
+        newList = [savedData objectForKey:@"scores"];
+    } else {
+        /// Si le fichier n'existe pas, on le créé
+        [[NSFileManager defaultManager] createFileAtPath:filePath contents:nil attributes:nil];
+    }
+
+    
+    
     [self.tableView reloadData];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 0;
+    return newList.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"default" forIndexPath:indexPath];
-    //cell.textLabel.text = book.title;
-    //cell.detailTextLabel.text = [NSString stringWithFormat:@"%@ %@", book.author.firstname, book.author.lastname];
+    NSArray* values = [newList allValues];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@", [[values objectAtIndex:indexPath.row] valueForKey:@"score"]];
     
     return cell;
 }
 
 
 - (IBAction)backButton:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
+
+- (void) addScore:(NSUInteger)value {
+    NSDictionary* datas = @{
+                            @"score":@(value)
+                            };
+    
+    /// On ajoute les nouvelles données à la suite de la liste existante
+    [newList setObject:datas forKey:[NSString stringWithFormat:@"%@", @(newList.count + 1)]];
+    
+    
+    /// Récupération des données du fichier dans un dictionnaire
+    NSMutableDictionary *dataDict = [[NSMutableDictionary alloc] init];
+    [dataDict setObject:newList forKey:@"scores"];
+    
+    /// Réécriture dans le fichier avec les nouveaux éléments
+    [dataDict writeToFile:filePath atomically:YES];
+
+}
+
 @end
